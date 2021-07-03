d76e305040e8546fecae635d5ba21a6637617369
Takuya-Endo <above5purple@gmail.com>
2021-07-03 18:42:19 +0900

    [Add]Chapter05.java

---
+++ Variable/Variable_practice/src/Chapter05.java
@@ -0,0 +1,35 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		
+		long var1 = 1234567890L;
+		double var2 = 3.14D;
+		boolean var3 = true;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		
+		Float var4 = 3.14F;
+		Integer var5 = 1;
+		Character var6 = 'あ';
+		
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+		
+		int[] array = new int[5];
+		array[0] = 10;
+		array[1] = 20;
+		array[2] = 30;
+		array[3] = 40;
+		array[4] = 50;
+		
+		int index3 = array[3];
+		
+		System.out.println(index3);
+		
+	}
+	
+}
