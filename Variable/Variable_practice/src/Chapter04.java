c911d261342d495db8ca4279984085fd85f2ec6e
Takuya-Endo <above5purple@gmail.com>
2021-07-03 17:45:07 +0900

    [Add]Chapter04.java

---
+++ Variable/Variable_practice/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] arg) {
+		String name = "Takuya Endo";
+		System.out.println(name);
+		
+		name = "Endo Takuya";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 9;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
