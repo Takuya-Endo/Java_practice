35f055c9695dd374c6251d153a40924dc4c839c5
Takuya-Endo <above5purple@gmail.com>
2021-07-03 17:37:41 +0900

    [Add]Variable02.java

---
+++ Variable/Variable_practice/src/Variable02.java
@@ -0,0 +1,12 @@
+
+public class Variable02 {
+	
+	public static void main(String[] arg) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+
+//エラー		final int MIN_NUMBER = 20;
+//エラー		System.out.println(MIN_NUMBER);
+	}
+	
+}
