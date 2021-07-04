62dad675bf9a94b323dcebdebcd1d9e291fbcc16
Takuya-Endo <above5purple@gmail.com>
2021-07-04 21:11:51 +0900

    [Add]Chapter07.java問題2

--- Function/Function_practice/src/Chapter07.java
+++ Function/Function_practice/src/Chapter07.java
@@ -1,7 +1,7 @@
 
 public class Chapter07 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		
 		int distance = 12;
 		
@@ -17,6 +17,23 @@ public class Chapter07 {
 		else {
 			System.out.println("とても遠いです");
 		}
+		
+		String signal_color = "green";
+		
+		switch (signal_color) {
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		case "yellow":
+			System.out.println("黄信号です");
+			break;
+		case "blue":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の色ではありません");
+		}
+		
 	}
 	
 }
