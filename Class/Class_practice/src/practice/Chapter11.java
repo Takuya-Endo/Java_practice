2af57204ad3787dff217b831cd357a3e34241e6b
Takuya-Endo <above5purple@gmail.com>
2021-07-09 23:35:35 +0900

    [Add]Chapter11.java,Question

--- Class/Class_practice/src/practice/Chapter11.java
+++ Class/Class_practice/src/practice/Chapter11.java
@@ -1,5 +1,56 @@
 package practice;
 
 public class Chapter11 {
+	
+	public static void main(String[] args) {
+		String q1 = "クラスのメンバーは、何と何ですか？";
+		String a1 = "";
+		
+		String q2 = "アクセス修飾子は、何種類ありますか？";
+		String a2 = "";
+		
+		String q3 = "アクセス修飾子で、クラスに定義できるのは何と何ですか？";
+		String a3 = "";
+		
+		String q4 = "アクセス修飾子で、クラスに定義できるのは何と何ですか？";
+		String a4 = "";
+		
+		String q5 = "ディレクトリ階層でクラスをグループ化するしくみを何と呼びますか？";
+		String a5 = "";
+		
+		String q6 = "クラスで定義された変数を何変数と呼びますか？";
+		String a6 = "";
+		
+		String q7 = "メソッド内で定義された変数を何変数と呼びますか？";
+		String a7 = "";
+		
+		String q8 = "クラスから直接呼び出しができるメソッドを何と呼びますか？";
+		String a8= "";
+		
+		String q9 = "チェックする必要がない例外を何と呼びますか？";
+		String a9 = "";
+		
+		String q10 = "オブジェクト指向は、プログラムを何に見立ててプログラミングしますか？";
+		String a10 = "";
+		
+		String q11 = "Javaで「型」というと、何を指しますか？";
+		String a11 = "";
+		
+		String q12 = "クラスを実体化したものを何と呼びますか？";
+		String a12 = "";
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a7);
+		System.out.println(a8);
+		System.out.println(a9);
+		System.out.println(a10);
+		System.out.println(a11);
+		System.out.println(a12);
+	}
 
 }
