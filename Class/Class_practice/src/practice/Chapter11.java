5f1ab6284e9b361e819ab27a9de40a5103288c60
Takuya-Endo <above5purple@gmail.com>
2021-07-11 21:24:14 +0900

    [Add]Chapter11.java,Answer

--- Class/Class_practice/src/practice/Chapter11.java
+++ Class/Class_practice/src/practice/Chapter11.java
@@ -4,40 +4,43 @@ public class Chapter11 {
 	
 	public static void main(String[] args) {
 		String q1 = "クラスのメンバーは、何と何ですか？";
-		String a1 = "";
+		String a1 = "フィールドとメソッド";
 		
 		String q2 = "アクセス修飾子は、何種類ありますか？";
-		String a2 = "";
+		String a2 = "「public」と「protected」と「package-private」と「private」の4種類";
 		
 		String q3 = "アクセス修飾子で、クラスに定義できるのは何と何ですか？";
-		String a3 = "";
+		String a3 = "publicとpackage-private";
 		
-		String q4 = "アクセス修飾子で、クラスに定義できるのは何と何ですか？";
-		String a4 = "";
+		String q4 = "インスタンス生成時に呼び出されるメソッドを何と呼びますか？";
+		String a4 = "コンストラクタ";
 		
 		String q5 = "ディレクトリ階層でクラスをグループ化するしくみを何と呼びますか？";
-		String a5 = "";
+		String a5 = "パッケージ";
 		
 		String q6 = "クラスで定義された変数を何変数と呼びますか？";
-		String a6 = "";
+		String a6 = "メンバ変数";
 		
 		String q7 = "メソッド内で定義された変数を何変数と呼びますか？";
-		String a7 = "";
+		String a7 = "ローカル変数";
 		
 		String q8 = "クラスから直接呼び出しができるメソッドを何と呼びますか？";
-		String a8= "";
+		String a8 = "static（スタティック）メソッド";
 		
 		String q9 = "チェックする必要がない例外を何と呼びますか？";
-		String a9 = "";
+		String a9 = "ランタイム例外（RuntimeException）";
 		
 		String q10 = "オブジェクト指向は、プログラムを何に見立ててプログラミングしますか？";
-		String a10 = "";
+		String a10 = "もの（オブジェクト）";
 		
 		String q11 = "Javaで「型」というと、何を指しますか？";
-		String a11 = "";
+		String a11 = "クラス";
 		
 		String q12 = "クラスを実体化したものを何と呼びますか？";
-		String a12 = "";
+		String a12 = "インスタンス";
+		
+		String q13 = "インスタンスを生成することを何と呼びますか？";
+		String a13 = "インスタンス化";
 		
 		System.out.println(a1);
 		System.out.println(a2);
