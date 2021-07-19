ed32a7fa2379028878805a8ee25f32bf1ca098bf
Takuya-Endo <above5purple@gmail.com>
2021-07-19 16:23:46 +0900

    [Update]Q_0101.java

--- Question/Question_practice/src/Q_0101.java
+++ Question/Question_practice/src/Q_0101.java
@@ -1,4 +1,14 @@
+//10から1まで数え、偶数のみを表示する
 
 public class Q_0101 {
+	
+	public static void main(String[] args) {
+		
+		for (int number = 10; number >= 1; number--) {
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+	}
 
 }
