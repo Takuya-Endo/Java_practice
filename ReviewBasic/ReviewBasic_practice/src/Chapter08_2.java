1f222eab114f0eb65048de044eaf97d1f4ba1277
Takuya-Endo <above5purple@gmail.com>
2021-07-20 22:31:50 +0900

    [Update]Chapter08-2_Q-03.java

--- ReviewBasic/ReviewBasic_practice/src/Chapter08_2.java
+++ ReviewBasic/ReviewBasic_practice/src/Chapter08_2.java
@@ -10,9 +10,19 @@ public class Chapter08_2 {
 		}
 		
 		
-		int[] array = {10, 20, 30, 40};
+		int[] array = {10, 15, 20, 25};
 		for (int element_number = 0; element_number < array.length; element_number++) {
 			System.out.println(array[element_number]);
+		}
+		
+		
+		for (int element_value : array) {
+			if (element_value % 2 == 0) {
+				continue;
+			} else if (element_value % 2 != 0) {
+				System.out.println(element_value);
+			}
+			
 		}
 		
 	}
