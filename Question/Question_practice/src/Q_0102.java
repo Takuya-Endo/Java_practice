5504982195b8be1d21ac6ab48a811cda1e82dd56
Takuya-Endo <above5purple@gmail.com>
2021-07-20 21:55:07 +0900

    [Update]Q_0102.java

--- Question/Question_practice/src/Q_0102.java
+++ Question/Question_practice/src/Q_0102.java
@@ -2,4 +2,23 @@
 
 public class Q_0102 {
 
+	public static void main(String[] args) {
+		
+		int even_number_count = 0;
+		int even_number_total = 0;
+		
+		for (int number = 1; number <= 10; number++) {
+			if (number % 2 == 0) {
+				
+				even_number_count += 1;
+				even_number_total += number;
+				
+			}
+		}
+		
+		System.out.println("偶数の個数は " + even_number_count + " です");
+		System.out.println("偶数の合計は " + even_number_total + " です");
+		
+	}
+	
 }
