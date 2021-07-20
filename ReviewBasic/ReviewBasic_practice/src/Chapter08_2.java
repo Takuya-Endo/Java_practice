515005ef8c34476b541f6d19bb90e704e1312311
Takuya-Endo <above5purple@gmail.com>
2021-07-20 22:17:40 +0900

    [Add]Chapter08-2.java

---
+++ ReviewBasic/ReviewBasic_practice/src/Chapter08_2.java
@@ -0,0 +1,14 @@
+
+public class Chapter08_2 {
+	
+	public static void main(String[] args) {
+		
+		int while_number = 1;
+		while (while_number < 5) {
+			System.out.println(while_number * while_number);
+			++while_number;
+		}
+		
+	}
+
+}
