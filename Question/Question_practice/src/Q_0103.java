b764b6da6765694888b077d9c74239b88ddf21ed
Takuya-Endo <above5purple@gmail.com>
2021-07-20 21:58:47 +0900

    [Update]Q_0103.java

--- Question/Question_practice/src/Q_0103.java
+++ Question/Question_practice/src/Q_0103.java
@@ -1,5 +1,19 @@
 //1から10までをカンマ区切りで表示（10の後には付けない）
 
 public class Q_0103 {
+	
+	public static void main(String[] args) {
+		
+		for (int number = 1; number <= 10; number++) {
+			
+			if (number == 10) {
+				System.out.print(number);
+			} else {
+				System.out.print(number + ", ");
+			}
+			
+		}
+		
+	}
 
 }
