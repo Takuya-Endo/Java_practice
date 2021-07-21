e2a54fc68f28afa0040821f3657092977d5fb3b3
Takuya-Endo <above5purple@gmail.com>
2021-07-21 23:20:29 +0900

    [Add]Car-3.java

---
+++ ReviewBasic/ReviewBasic_practice/src/practice/Car_3.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Car_3 {
+	
+    private double fuelCost; // 燃費（Km/L）
+    private double fuelAmount; // 残量（L）
+
+    // コンストラクタを作成
+
+    // moveメソッド
+    // ・"xx km 走ります"を出力
+    // ・残量を計算
+
+    // fuelAmountを取得するメソッドを作成
+
+}
