94976cd9906b5a1fbc7e02e1c37af568d72aa987
Takuya-Endo <above5purple@gmail.com>
2021-07-23 09:03:41 +0900

    [Add]Human03-2.java

--- ReviewBasic/ReviewBasic_practice/src/practice/ClassMethod03_2.java
+++ ReviewBasic/ReviewBasic_practice/src/practice/ClassMethod03_2.java
@@ -3,7 +3,7 @@ package practice;
 public class ClassMethod03_2 {
 	
 	public static void main(String[] args) {
-        Human03 yamada = new Human03();
+        Human03_2 yamada = new Human03_2();
 
         System.out.println("私の名前は、" + yamada.name + "です。");
         String profile = yamada.getProfile();
