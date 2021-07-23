c15ba6204bd160f9c11612794a8bc365163d05cb
Takuya-Endo <above5purple@gmail.com>
2021-07-23 20:06:34 +0900

    [Update]ClassMethod03-2.java実行内容確認コメントアウト

--- ReviewBasic/ReviewBasic_practice/src/practice/ClassMethod03_2.java
+++ ReviewBasic/ReviewBasic_practice/src/practice/ClassMethod03_2.java
@@ -3,7 +3,14 @@ package practice;
 public class ClassMethod03_2 {
 	
 	public static void main(String[] args) {
-        Human03_2 yamada = new Human03_2();
+        
+		Human03_2 yamada = new Human03_2();
+        //コンストラクタHuman03_2を使って、インスタンスyamadaを生成
+		//メソッド⊃コンストラクタ
+		//コンストラクタ→メンバ変数の初期化など、クラスをインスタンス化したときの定型処理を、
+		//クラスの機能として実行してくれる特別なメソッド
+		//インスタンス化したときに一度だけ実行される
+		//コンストラクタ（constructor：建設者）
 
         System.out.println("私の名前は、" + yamada.name + "です。");
         String profile = yamada.getProfile();
--- ReviewBasic/ReviewBasic_practice/src/practice/Human03_2.java
+++ ReviewBasic/ReviewBasic_practice/src/practice/Human03_2.java
@@ -11,17 +11,30 @@ public class Human03_2 {
         this.age = 20;
         this.profession = "プログラマー";
     }
+    //コンストラクタ
+    //public			コンストラクタ名()
+    //メソッド⊃コンストラクタ
+  	//コンストラクタ→メンバ変数の初期化など、クラスをインスタンス化したときの定型処理を、
+  	//クラスの機能として実行してくれる特別なメソッド
+  	//インスタンス化したときに一度だけ実行される
+  	//コンストラクタ（constructor：建設者）
 
-    //String型の戻り値を返す引数なしのメソッドを定義
+    
     public String getProfile() {
         return "年齢は" + this.age + "、職業は" + this.profession;
     }
+    //メソッド
+    //public 戻り値の型 メソッド名(ある場合は引数)
+    //return 戻り値;
+    
 
     //戻り値を返さない、String型の引数を1つとるメソッドを定義
     public void greet(String friend) {
         if (friend == null) { //String型の値はnullの可能性があるのでチェック
             System.out.println("挨拶する友達がわかりません！");
             return;
+            //return文が実行された時点でメソッドを終了し呼び出し元へ処理が帰る
+            //return文が実行されるとそれ以降にメソッド内に書かれた処理は実行されない
         }
         System.out.println(friend + "さん、こんにちは！");
     }
