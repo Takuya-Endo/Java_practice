916b9d0bc33017e18432d2795a33314178afafe5
Takuya-Endo <above5purple@gmail.com>
2021-07-24 11:31:23 +0900

    [Update]Chapter09-3-capsule.javaカプセル化コメントアウト

--- ReviewBasic/ReviewBasic_practice/src/practice/Car_3_capsule.java
+++ ReviewBasic/ReviewBasic_practice/src/practice/Car_3_capsule.java
@@ -2,9 +2,11 @@ package practice;
 
 public class Car_3_capsule {
 	
-    public double fuelCost;
-    public double fuelAmount;
-
+//  public double fuelCost;	//外部（mainメソッド）で
+//  public double fuelAmount;	//変更が加えられないようprivateに
+    private double fuelCost;
+    private double fuelAmount;
+    
     public Car_3_capsule(double fuelCost, double fuelAmount) {
     	this.fuelCost = fuelCost;
     	this.fuelAmount = fuelAmount;
@@ -14,7 +16,11 @@ public class Car_3_capsule {
     	System.out.println(distance + "km走ります");
     	this.fuelAmount -= distance / this.fuelCost;
     }
-
+    
+    //privateとするとmainメソッドでcar.fuelAmountで値取得が出来ないため、
+    //新たに値取得用のメソッドを用意する
+    //例：値の取得「getter」メソッド　getフィールド名
+    //例：値の変更「setter」メソッド　setフィールド名
     public double getFuelAmount() {
     	return this.fuelAmount;
     }
--- ReviewBasic/ReviewBasic_practice/src/practice/Chapter09_3_capsule.java
+++ ReviewBasic/ReviewBasic_practice/src/practice/Chapter09_3_capsule.java
@@ -10,6 +10,12 @@ public class Chapter09_3_capsule {
         Car_3_capsule car = new Car_3_capsule(fuelCost, fuelAmount);
         
         car.move(20);
+        
+//      System.out.println("残量は、" + car.fuelAmount + "です");	//同じように表示可能
+//      car.fuelCost = 500;											//が、このように変更が加えられる恐れがある
+//      car.fuelAmount = 500;										//そこでクラスのアクセス修飾子をprivateに
+        //privateによって↑これで変更が加えられなくなったが、同時にSystem.out.printlnでの表示もcar.fuelAmountでは不可に
+        //そこで、値取得のメソッドgetFuelAmount()を作成する↓
         System.out.println("残量は、" + car.getFuelAmount() + "です");
     }
 
