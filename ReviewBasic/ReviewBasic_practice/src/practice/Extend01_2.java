6043e39bed0d3592e6a8ec1c6153034fc1298ef0
Takuya-Endo <above5purple@gmail.com>
2021-07-27 05:34:14 +0900

    [Update]Extend01-2.javaコメントアウト：ダウンキャスト、アップキャスト

--- ReviewBasic/ReviewBasic_practice/src/practice/Extend01_2.java
+++ ReviewBasic/ReviewBasic_practice/src/practice/Extend01_2.java
@@ -16,19 +16,36 @@ public class Extend01_2 {
         //scoreはEx_Student_2のコンストラクタで新たに作成
         
         
+        //キャスト→型の変換を行うこと (データ型) 式
+        
+        Ex_Employee_2 employee = new Ex_Employee_2("田中", 28, "システム部");
+        
         //アップキャスト
-        Ex_Human_2 human = new Ex_Employee_2("田中", 28, "システム部");
+        //サブクラスのインスタンスemployeeをスーパークラスの変数humanに代入
+        Ex_Human_2 human = 				employee;	//アップキャストの場合は型を指定しなくても自動で行われる
+      //Ex_Human_2 human = (Ex_Human_2) employee;
+        
+        //	↑
+        //1行でも記述可能
+        //Employeeクラスのインスタンスを生成して、アップキャストでHumanクラスの変数に代入
+        //Ex_Human_2 human = new Ex_Employee_2("田中", 28, "システム部");
+        //	↑					↑
+        //	↑				Ex_Employee_2 employee = new Ex_Employee_2("田中", 28, "システム部");
+        //Ex_Human_2 human = (Ex_Human_2) employee;
+        
         System.out.println("Humanクラスのメソッド：名前は、" + human.getName());
-
-        Employee tanaka = (Employee) human;
+        
+        //ダウンキャスト
+        //スーパークラスhumanからサブクラスtanakaへ変換
+        Ex_Employee_2 tanaka = (Ex_Employee_2) human;	//型の指定が必要
         System.out.println("Employeeクラスのメソッド：プロフィールを紹介します。" + tanaka.getEmployeeProfile());
 
-        Extend01.printName(sugimoto);
-        Extend01.printName(sato);
-        Extend01.printName(tanaka);
+        Extend01_2.printName(sugimoto);
+        Extend01_2.printName(sato);
+        Extend01_2.printName(tanaka);
     }
 
-    public static void printName(Human human) {
+    public static void printName(Ex_Human_2 human) {
         System.out.println("Humanクラスのメソッド：名前は、" + human.getName());
     }
 
