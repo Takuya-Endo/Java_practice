ddcf7a9d625ed85d2b42f1d7f0fa3c93e66ed054
Takuya-Endo <above5purple@gmail.com>
2022-02-26 20:46:41 +0900

    [Update]JavaSilverPractice-3

--- JavaSilverPractice/src/sample/SampleMain.java
+++ JavaSilverPractice/src/sample/SampleMain.java
@@ -5,9 +5,14 @@ public class SampleMain {
 	public static void main(String[] args) {
 		
 		String ___ = "アンダースコアのみ識別子（変数名）";
-		
 		System.out.println(___);
-
+		
+		float valueA = 3.14f;
+		String valueB = "true";
+		System.out.println(valueA + valueB);
+		
+		System.out.println(10 + 20 + "30" + 40);
+		
 	}
 
 }
