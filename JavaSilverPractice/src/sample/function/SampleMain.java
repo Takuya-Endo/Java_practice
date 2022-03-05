8bc8ff0aa53f926306e2acc829c07a0abb793e93
Takuya-Endo <above5purple@gmail.com>
2022-03-05 18:40:43 +0900

    [Update]JavaSilverPractice-16

--- JavaSilverPractice/src/sample/function/SampleMain.java
+++ JavaSilverPractice/src/sample/function/SampleMain.java
@@ -5,23 +5,48 @@ public class SampleMain {
 	public static void main(String[] args) {
 		
 		String value = "";
+		FunctionTypeInterface sampleClassPolymorphism;
 		
 		SampleClassImplementsA sampleClassA = new SampleClassImplementsA();
 		value = sampleClassA.sampleFunction("TestString");
 		System.out.println(value);
 		
-		FunctionTypeInterface sampleClassPolymorphismA = new SampleClassImplementsA();
-		value = sampleClassPolymorphismA.sampleFunction("TestString");
+		sampleClassPolymorphism = new SampleClassImplementsA();
+		value = sampleClassPolymorphism.sampleFunction("TestString");
 		System.out.println(value);
 		
 		SampleClassImplementsB sampleClassB = new SampleClassImplementsB();
 		value = sampleClassB.sampleFunction("TestString");
 		System.out.println(value);
 		
-		FunctionTypeInterface sampleClassPolymorphismB = new SampleClassImplementsB();
-		value = sampleClassPolymorphismB.sampleFunction("TestString");
+		sampleClassPolymorphism = new SampleClassImplementsB();
+		value = sampleClassPolymorphism.sampleFunction("TestString");
 		System.out.println(value);
 		
+		SampleMain sampleMain = new SampleMain();
+		value = sampleMain.mainPrivateFunction("TestString");
+		System.out.println(value);
+		
+//		sampleClassPolymorphism = sampleMain.mainPrivateFunction("TestString");
+		sampleClassPolymorphism = sampleMain::mainPrivateFunction;
+		sampleClassPolymorphism.sampleFunction("TestString");
+		System.out.println(value);
+		
+		value = SampleMain.mainStaticFunction("TestString");
+		System.out.println(value);
+		
+		sampleClassPolymorphism = SampleMain::mainStaticFunction;
+		sampleClassPolymorphism.sampleFunction("TestString");
+		System.out.println(value);
+		
+	}
+	
+	private String mainPrivateFunction(String argument) {
+		return argument + "（Mainクラスのprivateメソッドを利用）";
+	}
+	
+	public static String mainStaticFunction(String argument) {
+		return argument + "（Mainクラスのstaticメソッドを利用）";
 	}
 
 }
