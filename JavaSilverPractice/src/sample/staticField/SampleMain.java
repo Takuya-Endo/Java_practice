440c6fd8885ca783682d71f01256480ac4bef6ee
Takuya-Endo <above5purple@gmail.com>
2022-03-13 08:27:07 +0900

    [Update]JavaSilverPractice-36

--- JavaSilverPractice/src/sample/staticField/SampleMain.java
+++ JavaSilverPractice/src/sample/staticField/SampleMain.java
@@ -4,11 +4,21 @@ public class SampleMain {
 	
 	private static int number;
 	
+//	public SampleMain(int number) {
+//		this.number = number * 2;
+//	}
+	static {
+		number = 10;
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println(number);
 //		System.out.println(SampleMain.number); //OK
 //		System.out.println(this.number); //NG
+
+//		SampleMain main = new SampleMain(5); //コンストラクタにすると前後でstaticの出力値が変わる→static初期価値を使わないと記述準によって値が変わる。
+		System.out.println(number);
 		
 		
 	}
