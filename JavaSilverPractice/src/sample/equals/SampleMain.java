86a6cb4d68761ce4bb42707e4815ecaa9a3b3471
Takuya-Endo <above5purple@gmail.com>
2022-03-16 07:13:44 +0900

    [Update]JavaSilverPractice-40

--- JavaSilverPractice/src/sample/equals/SampleMain.java
+++ JavaSilverPractice/src/sample/equals/SampleMain.java
@@ -4,6 +4,12 @@ public class SampleMain {
 
 	public static void main(String[] args) {
 		
+		SampleA sampleA = new SampleA("SampleA");
+		SampleB sampleB = new SampleB("SampleB");
+		
+		System.out.println(sampleA.equals(sampleB));
+		System.out.println(sampleB.equals(sampleA));
+		
 	}
 	
 }
