aad87490d85dec2211fa99c78cc7e2558050260c
Takuya-Endo <above5purple@gmail.com>
2022-03-17 07:51:30 +0900

    [Update]JavaSilverPractice-41

--- JavaSilverPractice/src/sample/equals/SampleMain.java
+++ JavaSilverPractice/src/sample/equals/SampleMain.java
@@ -10,6 +10,15 @@ public class SampleMain {
 		System.out.println(sampleA.equals(sampleB));
 		System.out.println(sampleB.equals(sampleA));
 		
+		
+		SampleA sampleA_1 = new SampleA("SampleA_1");
+		SampleA sampleA_2 = new SampleA("SampleA_2");
+		SampleB sampleB_1 = new SampleB("SampleB_1");
+		SampleB sampleB_2 = new SampleB("SampleB_2");
+		
+		System.out.println(sampleA_1.equals(sampleA_2));
+		System.out.println(sampleB_1.equals(sampleB_2));
+		
 	}
 	
 }
