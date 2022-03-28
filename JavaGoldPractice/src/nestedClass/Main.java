0ffa913c34bd4bb92dec55d71e3b4fac0705f1f4
Takuya-Endo <above5purple@gmail.com>
2022-03-29 07:54:59 +0900

    [Update]JavaGoldPractice-5

--- JavaGoldPractice/src/nestedClass/Main.java
+++ JavaGoldPractice/src/nestedClass/Main.java
@@ -1,10 +1,21 @@
 package nestedClass;
 
+import nestedClass.sampleConcealment.SampleFactory;
+import nestedClass.sampleConcealment.SampleImplements;
+import nestedClass.sampleConcealment.SampleInterface;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("InnerClass");
+		SampleImplements sampleImplementsA = new SampleImplements();
+		sampleImplementsA.conceal();
+		sampleImplementsA.publish();
+		
+//		SampleImplements sampleImplementsB = SampleFactory.create();
+		SampleInterface sampleImplementsB = SampleFactory.create();
+//		sampleImplementsB.conceal();
+		sampleImplementsB.publish();
 		
 	}
 
