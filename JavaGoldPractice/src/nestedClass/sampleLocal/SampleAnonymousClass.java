b8342e01e60a284e3fbf5873012c0c263054a389
Takuya-Endo <above5purple@gmail.com>
2022-03-30 07:56:29 +0900

    [Update]JavaGoldPractice-9

---
+++ JavaGoldPractice/src/nestedClass/sampleLocal/SampleAnonymousClass.java
@@ -0,0 +1,14 @@
+package nestedClass.sampleLocal;
+
+public class SampleAnonymousClass {
+	
+	public void sampleMethod() {
+		
+		//ローカルクラスを、どうせnewするだけという理由で匿名クラスに書き換えることはできない。
+		//ローカルクラスが自作だった場合、名前がなくなるため、コンパイルエラーとなる。
+		//そのためインターフェース実装や抽象クラスを継承したクラス等のみ匿名クラスとして使える。
+		
+		
+	}
+	
+}
