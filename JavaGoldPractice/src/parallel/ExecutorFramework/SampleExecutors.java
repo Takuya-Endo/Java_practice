e8642a17363f28a8c6dcac5f150925fa4ac93c51
Takuya-Endo <above5purple@gmail.com>
2022-04-01 08:01:04 +0900

    [Update]JavaGoldPractice-16

--- JavaGoldPractice/src/parallel/ExecutorFramework/SampleExecutors.java
+++ JavaGoldPractice/src/parallel/ExecutorFramework/SampleExecutors.java
@@ -1,9 +1,14 @@
 package parallel.ExecutorFramework;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SampleExecutors {
+	
+	//Executorsクラスは
+	//ExecutorServiceインターフェースやScheduledExecutorServiceインターフェースを
+	//実現するためのFactoryクラス。create用のstaticメソッドがいくつか用意されている。
 	
 	public void doSomething() {
 		
@@ -16,15 +21,33 @@ public class SampleExecutors {
 			System.out.print(id + " ");
 		};
 		
-		//ExecutorServiceインターフェースを実現するためのFactoryクラス
-		//create用のstaticメソッドがいくつか用意されている
+		//①新しいスレッドを一つ生成
+//		ExecutorService executorService_1 = Executors.newSingleThreadExecutor();
+//		for (int i = 0; i < 5; i++) {
+//			executorService_1.submit(task);
+//		}
+		
+		//②新しいスレッドを引数の数だけ生成（Fixed＝固定数）
+//		ExecutorService executorService_2 = Executors.newFixedThreadPool(3);
+//		for (int i = 0; i < 5; i++) {
+//			executorService_2.submit(task);
+//		}
+		
+		//③新しいスレッドを必要に応じて生成
+//		ExecutorService executorService_3 = Executors.newCachedThreadPool();
+//		
+//		for (int i = 0; i < 5; i++) {
+//			executorService_3.submit(task);
+//		}
+		
+		//ScheduledExecutorServiceインターフェースのscheduleメソッドは、
+		//第一引数のタスクを、第二引数の時間（単位は第三引数）経過後に、実行する。
 		
 		//①新しいスレッドを一つ生成
-		ExecutorService executorService_1 = Executors.newSingleThreadExecutor();
+		ScheduledExecutorService scheduledExecutorService_1 = Executors.newSingleThreadScheduledExecutor();
 		for (int i = 0; i < 5; i++) {
-			executorService_1.submit(task);
+			scheduledExecutorService_1.schedule(task, 3, TimeUnit.SECONDS);
 		}
-		System.out.println();
 		
 	}
 
