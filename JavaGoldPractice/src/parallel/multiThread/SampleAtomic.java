b74349c0773368e939460139e38cf994cb918a11
Takuya-Endo <above5purple@gmail.com>
2022-04-12 07:09:26 +0900

    [Update]JavaGoldPractice-31

--- JavaGoldPractice/src/parallel/multiThread/SampleAtomic.java
+++ JavaGoldPractice/src/parallel/multiThread/SampleAtomic.java
@@ -1,35 +1,34 @@
 package parallel.multiThread;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SampleAtomic {
 
-	private int count = 0;
+	private AtomicInteger atomicInteger;
+	
+//	private int count = 0;
 	public void doSomething01() {
-
-		ExecutorService executorService = Executors.newFixedThreadPool(3);
-		
-		Runnable barrierAction = () -> System.out.println("3Thread");
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(3, barrierAction);
-		
-		Runnable task = () -> {
-			try {
-				
-				System.out.println("ID:" + Thread.currentThread().getId() + " COUNT:" + ++this.count);
-				cyclicBarrier.await();
-			
-			} catch (InterruptedException | BrokenBarrierException e) {
-				e.printStackTrace();
-			}
-		};
-		
-		for (int i = 0; i < 12; i++) {
-			executorService.submit(task);
-		}
-		executorService.shutdown();
+//
+//		ExecutorService executorService = Executors.newFixedThreadPool(3);
+//		
+//		Runnable barrierAction = () -> System.out.println("3Thread");
+//		CyclicBarrier cyclicBarrier = new CyclicBarrier(3, barrierAction);
+//		
+//		Runnable task = () -> {
+//			try {
+//				
+//				System.out.println("ID:" + Thread.currentThread().getId() + " COUNT:" + ++this.count);
+//				cyclicBarrier.await();
+//			
+//			} catch (InterruptedException | BrokenBarrierException e) {
+//				e.printStackTrace();
+//			}
+//		};
+//		
+//		for (int i = 0; i < 12; i++) {
+//			executorService.submit(task);
+//		}
+//		executorService.shutdown();
 
 	}
 	
