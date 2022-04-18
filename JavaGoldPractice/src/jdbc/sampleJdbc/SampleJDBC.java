599cde538271aa446537574fb6e87d7a12f6f654
Takuya-Endo <above5purple@gmail.com>
2022-04-19 07:24:16 +0900

    [Update]JavaGoldPractice-38

--- JavaGoldPractice/src/jdbc/sampleJdbc/SampleJDBC.java
+++ JavaGoldPractice/src/jdbc/sampleJdbc/SampleJDBC.java
@@ -11,17 +11,26 @@ public class SampleJDBC {
 //	>java -jar lib/derbyrun.jar server start
 	
 	public void doSomething01() {
+		
+		Connection connection = null;
+		
 		try {
 			
 //			ij> connect 'jdbc:derby://localhost:1527/data/Sample;';
-			Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/data/Sample");
+			connection = DriverManager.getConnection("jdbc:derby://localhost:1527/data/Sample");
 			
 			System.out.println(connection);
-			connection.close();
+			
 		
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}	
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
