cfb114b8e003cf8cf886ffc0d4764bffb6b13397
Takuya-Endo <above5purple@gmail.com>
2022-04-21 07:25:50 +0900

    [Update]JavaGoldPractice-40

--- JavaGoldPractice/src/jdbc/sampleJdbc/Main.java
+++ JavaGoldPractice/src/jdbc/sampleJdbc/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		new SampleJDBC().doSomething01();
+//		new SampleJDBC().doSomething01();
+		new SampleJDBC().doSomething02();
 
 	}
 
