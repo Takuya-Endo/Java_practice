5a23ae3d54a31389fc9131863bd58fe1d857995a
Takuya-Endo <above5purple@gmail.com>
2022-04-22 06:57:31 +0900

    [Update]JavaGoldPractice-41

--- JavaGoldPractice/src/jdbc/sampleJdbc/SampleJDBC.java
+++ JavaGoldPractice/src/jdbc/sampleJdbc/SampleJDBC.java
@@ -2,6 +2,7 @@ package jdbc.sampleJdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SampleJDBC {
@@ -39,6 +40,29 @@ public class SampleJDBC {
 		try (Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/data/Sample")) {
 			
 			System.out.println(connection);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void doSomething03() {
+		
+//		ij> connect 'jdbc:derby://localhost:1527/data/Sample;';
+		try (Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/data/Sample")) {
+			
+			String sql = "";
+			PreparedStatement preparedStatement = null;
+			
+			sql = "SELECT * FROM item;";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.close();
+			
+			sql = "INSERT INTO item (id, name) VALUES (?, ?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, 2);
+			preparedStatement.setString(2, "sample");
+			preparedStatement.close();
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
