f7a08aee1824c62b47bafc5c0980f962872051f9
Takuya-Endo <above5purple@gmail.com>
2022-04-23 09:50:38 +0900

    [Update]JavaGoldPractice-43

--- JavaGoldPractice/src/jdbc/sampleJdbc/Main.java
+++ JavaGoldPractice/src/jdbc/sampleJdbc/Main.java
@@ -8,7 +8,8 @@ public class Main {
 //		new SampleJDBC().doSomething02();
 //		new SampleJDBC().doSomething03();
 //		new SampleJDBC().doSomething04();
-		new SampleJDBC().doSomething05();
+//		new SampleJDBC().doSomething05();
+		new SampleJDBC().doSomething06();
 
 	}
 
--- JavaGoldPractice/src/jdbc/sampleJdbc/SampleJDBC.java
+++ JavaGoldPractice/src/jdbc/sampleJdbc/SampleJDBC.java
@@ -11,13 +11,13 @@ public class SampleJDBC {
 //	サーバー起動コマンド
 //	>cd C:\Users\home\Tools\derby\db-derby-10.15.2.0-bin
 //	>java -jar lib/derbyrun.jar server start
-	
 	public void doSomething01() {
 		
 		Connection connection = null;
 		
 		try {
 			
+//			コマンドプロンプトでSQLを入力する簡易クライアント起動は「>java -jar lib/derbyrun.jar ij」
 //			ij> connect 'jdbc:derby://localhost:1527/data/Sample;';
 			connection = DriverManager.getConnection("jdbc:derby://localhost:1527/data/Sample");
 			
@@ -109,6 +109,49 @@ public class SampleJDBC {
 					
 				}
 				
+			}
+			
+			//preparedStatementは明示的に閉じる必要あり
+			//resultSetも明示的に閉じる必要あり
+			//ただし、preparedStatementが閉じられると、resultSetも同時に閉じる
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void doSomething06() {
+		
+//		ij> connect 'jdbc:derby://localhost:1527/data/Sample;';
+		try (Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/data/Sample")) {
+			String sql = "";
+			
+			sql = "INSERT INTO item (id, name) VALUES (?, ?)";
+			try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+				String[] names = {"sampleC", "sampleD", "sampleE"};
+//				int result = 0;
+//				for (int i = 0; i < 3; i++) {
+//					preparedStatement.setInt(1, i + 3);
+//					preparedStatement.setString(2, names[i]);
+//					result += preparedStatement.executeUpdate();
+//				}
+//				System.out.println(result + "件更新");
+				for (int i = 0; i < 3; i++) {
+					preparedStatement.setInt(1, i + 3);
+					preparedStatement.setString(2, names[i]);
+					preparedStatement.addBatch();
+				}
+				int[] results = preparedStatement.executeBatch();
+				System.out.println(results.length + "件更新");
+			}
+			
+			sql = "SELECT * FROM item";
+			try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+				ResultSet resultSet = preparedStatement.executeQuery();
+				while (resultSet.next()) {
+					System.out.print(resultSet.getInt("id") + " ");
+					System.out.print(resultSet.getString("name") + "\n");
+				}
 			}
 			
 			//preparedStatementは明示的に閉じる必要あり
