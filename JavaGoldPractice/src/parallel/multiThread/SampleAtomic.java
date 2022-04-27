547be04473d3aa7fe04eb661e33548d76778cabd
Takuya-Endo <above5purple@gmail.com>
2022-04-28 07:44:07 +0900

    [Update]JavaGoldPractice-53

--- JavaGoldPractice/src/parallel/multiThread/SampleAtomic.java
+++ JavaGoldPractice/src/parallel/multiThread/SampleAtomic.java
@@ -5,12 +5,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class SampleAtomic {
 
 	private AtomicInteger atomicInteger;
 	private AtomicBoolean atomicBoolean;
 	private AtomicReference<String> atomicReferenceString;
+	private AtomicReferenceArray<String> atomicReferenceArray;
 	
 	private int count = 0;
 	public void doSomething01() {
