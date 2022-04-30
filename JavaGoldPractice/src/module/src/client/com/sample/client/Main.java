1067d3be10638a619d6afc12bc627083a0f8d3fd
Takuya-Endo <above5purple@gmail.com>
2022-04-30 10:00:49 +0900

    [Update]JavaGoldPractice-55

---
+++ JavaGoldPractice/src/module/src/client/com/sample/client/Main.java
@@ -0,0 +1,14 @@
+package module.src.com.sample.client;
+
+import module.src.com.sample.lib.Lib;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Lib lib = new Lib();
+		lib.test();
+
+	}
+
+}
---
+++ JavaGoldPractice/src/module/src/lib/com/sample/lib/Lib.java
@@ -0,0 +1,9 @@
+package com.sample.lib;
+
+public class Lib {
+	
+	public void test() {
+		System.out.println("test");
+	}
+
+}
