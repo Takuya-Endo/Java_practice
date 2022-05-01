2633b707ebe4438f99c502fd634294ad7b2b3744
Takuya-Endo <above5purple@gmail.com>
2022-05-01 23:09:57 +0900

    [Update]JavaGoldPractice-59

---
+++ JavaGoldPractice/src/streamAPI/Main.java
@@ -0,0 +1,14 @@
+package streamAPI;
+
+import streamAPI.optional.Proccess;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+//		new Proccess().doSomething01();
+		new Proccess().doSomething02();
+		
+	}
+
+}
---
+++ JavaGoldPractice/src/streamAPI/optional/Proccess.java
@@ -0,0 +1,50 @@
+package streamAPI.optional;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class Proccess {
+
+	//Optionalインスタンス生成
+	public void doSomething01() {
+		
+		//①nullを許容しない
+		Optional<String> optionalString01 = Optional.of("optionalString01");
+		System.out.println(optionalString01);
+		System.out.println(optionalString01.get());
+		
+		//NullPointerException
+//		Optional<String> optionalStringNull = Optional.of(null);
+		
+		
+		//②nullを許容する
+		Optional<String> optionalString02 = Optional.ofNullable("optionalString02");
+		System.out.println(optionalString02.get());
+		Optional<String> optionalStringNull = Optional.ofNullable(null); // = Optional<String> optionalStringNull = Optional.empty();
+		System.out.println(optionalStringNull); //Optional.empty
+//		System.out.println(optionalStringNull.get()); //java.util.NoSuchElementException
+		
+	}
+	
+	//Optionalインスタンスがnullか否か判定してから処理
+	public void doSomething02() {
+		
+//		Optional<String> optionalStringNull = Optional.ofNullable(null);
+		Optional<String> empty = Optional.empty();
+		
+		if (!empty.isPresent()) {
+			System.out.println(empty);
+//			System.out.println(empty.get()); //NoSuchElementException
+			System.out.println(empty.orElseGet(() -> "empty"));
+		}
+		
+		Consumer<String> consumer = str -> System.out.println(str);
+		Runnable runnable = () -> System.out.println("isEmpty");
+		empty.ifPresentOrElse(consumer, runnable);
+		
+		Optional<String> test = Optional.of("test");
+		test.ifPresent(consumer);
+		
+	}
+
+}
