a4e27301ab55e2ce13011c7c6c299edeb4d7ddf0
Takuya-Endo <above5purple@gmail.com>
2022-05-05 06:00:52 +0900

    [Update]JavaGoldPractice-68

--- JavaGoldPractice/src/streamAPI/stream/Process.java
+++ JavaGoldPractice/src/streamAPI/stream/Process.java
@@ -1,9 +1,15 @@
 package streamAPI.stream;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 
 public class Process {
 	
@@ -16,6 +22,31 @@ public class Process {
 		BinaryOperator<String> accumulator = (str1, str2) -> str1 + ", " + str2;
 		Optional<String> result = list.stream().reduce(accumulator);
 		System.out.println(result.get());
+		
+		Collector<String, StringBuilder, String> collector = new /* CollectorImpl implements */ Collector<String, StringBuilder, String>() {
+			@Override
+			public Supplier<StringBuilder> supplier() {
+				return () -> new StringBuilder();
+			}
+			@Override
+			public BiConsumer<StringBuilder, String> accumulator() {
+				return (stringBuilder, str) -> stringBuilder.append(str).append(", ");
+			}
+			@Override
+			public BinaryOperator<StringBuilder> combiner() {
+				return null;
+			}
+			@Override
+			public Function<StringBuilder, String> finisher() {
+				return stringBuilder -> stringBuilder.substring(0, stringBuilder.length() - 2);
+			}
+			@Override
+			public Set<Characteristics> characteristics() {
+				return new HashSet<Collector.Characteristics>();
+			}
+		};
+		String collectorResult = list.stream().collect(collector);
+		System.out.println(collectorResult);
 		
 	}
 
