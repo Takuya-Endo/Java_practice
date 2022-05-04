ed4b4bbbcbbc7b9cbd463e418611d3fbe8e21ee5
Takuya-Endo <above5purple@gmail.com>
2022-05-04 10:18:10 +0900

    [Update]JavaGoldPractice-67

--- JavaGoldPractice/src/streamAPI/stream/Process.java
+++ JavaGoldPractice/src/streamAPI/stream/Process.java
@@ -2,6 +2,8 @@ package streamAPI.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
 
 public class Process {
 	
@@ -9,6 +11,11 @@ public class Process {
 		
 		List<String> list = Arrays.asList("A", "B", "C", "D", "E");
 		list.stream().forEach(args -> System.out.print(args + " "));
+		System.out.print("\n");
+		
+		BinaryOperator<String> accumulator = (str1, str2) -> str1 + ", " + str2;
+		Optional<String> result = list.stream().reduce(accumulator);
+		System.out.println(result.get());
 		
 	}
 
