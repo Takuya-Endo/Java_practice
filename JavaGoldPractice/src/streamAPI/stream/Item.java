8d02dcff8e8356baad2ffd792c7bbaf52e21cfda
Takuya-Endo <above5purple@gmail.com>
2022-05-11 07:47:10 +0900

    [Update]JavaGoldPractice-80

---
+++ JavaGoldPractice/src/streamAPI/stream/Item.java
@@ -0,0 +1,28 @@
+package streamAPI.stream;
+
+public class Item {
+	
+	private String name;
+	private int price;
+	
+	public Item(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public int getPrice() {
+		return price;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
