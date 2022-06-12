22a4fd0c5bfa45244286dec7bbaf8e2802c2618d
Takuya-Endo <above5purple@gmail.com>
2022-06-12 12:25:56 +0900

    [Update]DesignPatternPractice-12

--- DesignPatternPractice/src/Iterator/EmployeesList.java
+++ DesignPatternPractice/src/Iterator/EmployeesList.java
@@ -4,8 +4,7 @@ public class EmployeesList implements Aggregate {
 
 	@Override
 	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new EmployeesListIterator();
 	}
 
 }
