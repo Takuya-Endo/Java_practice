f1b4f58e27ca56c59d48e2727365f9853a6c400f
Takuya-Endo <above5purple@gmail.com>
2022-06-28 07:47:29 +0900

    [Update]DesignPatternPractice-22

--- DesignPatternPractice/src/TemplateMethod/Super.java
+++ DesignPatternPractice/src/TemplateMethod/Super.java
@@ -1,5 +1,17 @@
 package TemplateMethod;
 
 public abstract class Super {
+	
+	public abstract void doSomething01();
+	public abstract void doSomething02();
+	public abstract void doSomething03();
 
+	public void callRoutine() {
+		
+		this.doSomething01();
+		this.doSomething02();
+		this.doSomething03();
+		
+	}
+	
 }
