61a700f38b715fbb47d0e95441f8231f6f6560cc
Takuya-Endo <above5purple@gmail.com>
2022-06-29 07:17:47 +0900

    [Update]DesignPatternPractice-23

--- DesignPatternPractice/src/TemplateMethod/Sub01.java
+++ DesignPatternPractice/src/TemplateMethod/Sub01.java
@@ -1,5 +1,20 @@
 package TemplateMethod;
 
-public class Sub01 {
+public class Sub01 extends Super {
+
+	@Override
+	public void doSomething01() {
+		System.out.println("Method_a");
+	}
+
+	@Override
+	public void doSomething02() {
+		System.out.println("Method_b");
+	}
+
+	@Override
+	public void doSomething03() {
+		System.out.println("Method_c");
+	}
 
 }
--- DesignPatternPractice/src/TemplateMethod/Sub02.java
+++ DesignPatternPractice/src/TemplateMethod/Sub02.java
@@ -1,5 +1,19 @@
 package TemplateMethod;
 
-public class Sub02 {
+public class Sub02 extends Super {
+	
+	@Override
+	public void doSomething01() {
+		System.out.println("Method_A");
+	}
 
+	@Override
+	public void doSomething02() {
+		System.out.println("Method_B");
+	}
+
+	@Override
+	public void doSomething03() {
+		System.out.println("Method_C");
+	}
 }
