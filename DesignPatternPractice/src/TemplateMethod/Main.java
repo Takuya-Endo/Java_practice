1bf81f5c551bf99cd1e6d16d4d395bc8f8f29352
Takuya-Endo <above5purple@gmail.com>
2022-06-30 07:14:41 +0900

    [Update]DesignPatternPractice-24

--- DesignPatternPractice/src/TemplateMethod/Main.java
+++ DesignPatternPractice/src/TemplateMethod/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		Super sub01 = new Sub01();
+		Super sub02 = new Sub02();
+		
+		sub01.callRoutine();
+		sub02.callRoutine();
+		
 	}
 	
 }
