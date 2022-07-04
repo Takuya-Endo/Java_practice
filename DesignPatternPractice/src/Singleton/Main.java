e5a9480d3efa2c1ec63f869d6d648cbc3221f529
Takuya-Endo <above5purple@gmail.com>
2022-07-05 07:19:29 +0900

    [Update]DesignPatternPractice-30

--- DesignPatternPractice/src/Singleton/Main.java
+++ DesignPatternPractice/src/Singleton/Main.java
@@ -6,11 +6,14 @@ public class Main {
 		
 //		SingletonSample singletonSample = new SingletonSample();
 		
-		SingletonSample singletonSample01 = SingletonSample.getSingletonSampleInstance();
-		SingletonSample singletonSample02 = SingletonSample.getSingletonSampleInstance();
+		SingletonSample singletonSample01 = SingletonSample.getSingletonSampleInstance("Instance01");
+		SingletonSample singletonSample02 = SingletonSample.getSingletonSampleInstance("Instance02");
 		
 		System.out.println(singletonSample01);
 		System.out.println(singletonSample02);
+		
+		System.out.println(singletonSample01.getInstanceName());
+		System.out.println(singletonSample02.getInstanceName());
 		
 	}
 
--- DesignPatternPractice/src/Singleton/SingletonSample.java
+++ DesignPatternPractice/src/Singleton/SingletonSample.java
@@ -2,13 +2,23 @@ package Singleton;
 
 public class SingletonSample {
 	
-	private static SingletonSample singletonSample = new SingletonSample();
+	private static String instanceName;
+	
+	private final static SingletonSample singletonSample = new SingletonSample();
 	
 	//privateで明示的にコンストラクタを用意しないと、newされてしまう
 	private SingletonSample() {}
 	
-	public static SingletonSample getSingletonSampleInstance() {
+	public static SingletonSample getSingletonSampleInstance(String instanceName) {
+		
+		if (SingletonSample.instanceName == null) {
+			SingletonSample.instanceName = instanceName;
+		}
 		return SingletonSample.singletonSample;
+	}
+	
+	public String getInstanceName() {
+		return SingletonSample.instanceName;
 	}
 	
 }
